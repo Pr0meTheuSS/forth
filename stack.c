d4f4d7fee6d98719152ca16deecec3bca5130b27
Yuriy Olimpiev <yr.olimpiev@gmail.com>
2026-09-24 15:39:07 +0700

    Implement stack, not checked yet

---
+++ stack.c
@@ -0,0 +1,38 @@
+
+#define maxStackSize 1024
+
+typedef struct Stack {
+    int topIndex;
+    int data[maxStackSize];
+} Stack;
+
+int push(Stack stack, int value) {
+ if (stack.topIndex >= maxStackSize) {
+   return -1;
+ }
+ 
+ stack.topIndex++;
+ stack.data[stack.topIndex] = value;
+ 
+ return 0;
+}
+
+typedef struct Ret {
+ int value;
+ int error;
+} Ret;
+
+Ret top(Stack stack) {
+ if (stack.topIndex < 0) {
+   return (Ret) {.value = 0, .error = -1};
+ }
+ return (Ret) {.value = stack.data[stack.topIndex], .error = 0 };
+}
+
+Ret pop(Stack stack) {
+ if (stack.topIndex < 0) {
+  return (Ret) {.value = stack.data[stack.topIndex], .error = 0 };
+ }
+ return (Ret) {.value = 0, .error = -1};
+}
+
