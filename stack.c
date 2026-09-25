e4a39da66c41a81be3a3d7fa43f735bcb6cd1d8e
Yuriy Olimpiev <yr.olimpiev@gmail.com>
2026-09-25 14:57:32 +0700

    Separate stack to .h and .c files

---
+++ stack.c
@@ -0,0 +1,27 @@
+#pragma once
+#include "stack.h"
+
+Ret push(Stack stack, int value) {
+  if (stack.topIndex >= maxStackSize) {
+    return (Ret){.value = 0, .error = -1};
+  }
+
+  stack.topIndex++;
+  stack.data[stack.topIndex] = value;
+
+  return (Ret){.value = value, .error = 0};
+}
+
+Ret top(Stack stack) {
+  if (stack.topIndex < 0) {
+    return (Ret){.value = 0, .error = -1};
+  }
+  return (Ret){.value = stack.data[stack.topIndex], .error = 0};
+}
+
+Ret pop(Stack stack) {
+  if (stack.topIndex < 0) {
+    return (Ret){.value = stack.data[stack.topIndex], .error = 0};
+  }
+  return (Ret){.value = 0, .error = -1};
+}
--- stack.h
+++ stack.h
@@ -6,32 +6,12 @@ typedef struct Stack {
   int data[maxStackSize];
 } Stack;
 
-int push(Stack stack, int value) {
-  if (stack.topIndex >= maxStackSize) {
-    return -1;
-  }
-
-  stack.topIndex++;
-  stack.data[stack.topIndex] = value;
-
-  return 0;
-}
 
 typedef struct Ret {
   int value;
   int error;
 } Ret;
 
-Ret top(Stack stack) {
-  if (stack.topIndex < 0) {
-    return (Ret){.value = 0, .error = -1};
-  }
-  return (Ret){.value = stack.data[stack.topIndex], .error = 0};
-}
-
-Ret pop(Stack stack) {
-  if (stack.topIndex < 0) {
-    return (Ret){.value = stack.data[stack.topIndex], .error = 0};
-  }
-  return (Ret){.value = 0, .error = -1};
-}
+Ret push(Stack stack, int value);
+Ret top(Stack stack);
+Ret pop(Stack stack);
